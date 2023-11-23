8ef78a2a642e4e9502b024ec6dcd604965544473
mylittleunicorn <nasir.dev48@gmail.com>
2023-11-23 12:59:19 +0700

    baru belajar operator

---
+++ learn/001Days.py
@@ -0,0 +1,50 @@
+# Ini Komen Pertama
+# Ini Komen kedua
+# Belajar menggunakan python
+
+"""kalo ini
+komentar dengan mutile line.
+jadi kalo mau membuat komentar
+yang banyak pakai ini saja"""
+
+# Tipe data di python
+print(2 + 3)                        # addition(+)
+print(3 - 1)                        # subtraction(-)
+print(2 * 3)                        # multiplication(*)
+print(3 / 2)                        # division(/)
+print(3 ** 6)                       # exponential(**)
+print(3 % 2)                        # modulus(%)
+print(4 // 2)                       # Floor division operator(//)
+
+# Cek tipe data
+print(type(10))                     # Int
+print(type(3.14))                   # Float
+print(type(1 + 3j))                 # Complex number
+print(type('Asabeneh'))             # String
+print(type([1, 2, 3]))              # List
+print(type(
+    {
+        'name' : 'Ucup D Maun',
+        'addres' : 'Indonesia'
+    }
+))                                  # Dictionary
+print(type({9.8, 3.14, 2.7}))       # Set
+print(type((9.8, 3.14, 2.7)))       # Tuple
+
+
+# ini variable
+nilai_pertama = 10
+nilai_kedua = 3
+var_dicti = {
+    'nama' : 'ucup d maun',
+    'usia' : 16,
+    'hobi' : [
+        'makan', 'minum'
+    ]
+}
+
+# LATIHAN
+print(nilai_pertama + nilai_kedua)
+print(nilai_pertama - nilai_kedua)
+print(nilai_pertama / nilai_kedua)
+print(var_dicti)
---
+++ learn/003Days-operators.py
@@ -0,0 +1,21 @@
+# Arithmetic Operations in Python
+# Integers
+
+print('Addition: ', 1 + 2)        # 3
+print('Subtraction: ', 2 - 1)     # 1
+print('Multiplication: ', 2 * 3)  # 6
+print ('Division: ', 4 / 2)       # 2.0  Division in Python gives floating number
+print('Division: ', 6 / 2)        # 3.0         
+print('Division: ', 7 / 2)        # 3.5
+print('Division without the remainder: ', 7 // 2)   # 3,  gives without the floating number or without the remaining
+print ('Division without the remainder: ',7 // 3)   # 2
+print('Modulus: ', 3 % 2)         # 1, Gives the remainder
+print('Exponentiation: ', 2 ** 3) # 9 it means 2 * 2 * 2
+
+# Floating numbers
+print('Floating Point Number, PI', 3.14)
+print('Floating Point Number, gravity', 9.81)
+
+# Complex numbers
+print('Complex number: ', 1 + 1j)
+print('Multiplying complex numbers: ',(1 + 1j) * (1 - 1j))
